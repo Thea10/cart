17baf441a83c4c8c5b1f2af21f65c3070b27a11b
Thea Abigail <olucynakin@gmail.com>
2019-09-14 01:02:26 +0100

    minor edits

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -6,27 +6,28 @@ class Cart extends Component {
             color: "#ffffff",
         }
 
-        const {cartItems,  handleRemoveFromCart, handleCheckout} = this.props;
+        const {cartItems, handleRemoveFromCart, handleCheckout} = this.props;
 
-        const totalPrice = cartItems.reduce((total, item)=>{
+        const totalPrice = cartItems.reduce((total, item) => {
             return total + (item.price)
         }, 0);
-
-        //console.log(totalPrice);
-        
-
         return (
             <div className="cart alert alert-info">
                 <h4> Cart </h4>
-
-                {cartItems.length === 0 ? <h5> Empty </h5> : <div> <h5> Items: {cartItems.length} </h5>
-                    <ul>
-                       <CartItemList items ={cartItems} remove={ handleRemoveFromCart}/>
-                    </ul>
-                    <h5> <b> Total- ${totalPrice} </b> </h5>
-                    <button className="btn btn-secondary" onClick={ (e) => handleCheckout(e) }>
-                        <a href = "#" style={{ color: style.color }}> CHECKOUT</a>
-                    </button> </div> }
+                {cartItems.length === 0 ?
+                    <h5> Empty </h5>
+                    :
+                    <div>
+                        <h5> Items: {cartItems.length} </h5>
+                        <ul>
+                            <CartItemList items ={cartItems} remove={ handleRemoveFromCart}/>
+                        </ul>
+                        <h5> <b> Total- ${totalPrice} </b> </h5>
+                        <button className="btn btn-secondary" onClick={ (e) => handleCheckout(e) }>
+                            <a href = "#" style={{ color: style.color }}> CHECKOUT</a>
+                        </button>
+                    </div>
+                }
 
             </div>
         )
@@ -34,18 +35,18 @@ class Cart extends Component {
 }
 
 function CartItemList(props) {
-     const cartitems = props.items.map(item => {
-       //  let itemtotal = item.price * item.count;
-         return(
-             <li key={item.id}> 
-             <h4> {item.title} - {item.count}  - ${item.price}  </h4>
-             <button className="btn btn-danger" onClick={ (e) => props.remove(e, item) } > Remove </button>
-             </li> 
-         )
-     })
-     return(
-         cartitems
-     )
+    const cartitems = props.items.map(item => {
+        //  let itemtotal = item.price * item.count;
+        return (
+            <li key={item.id}>
+                <h4> {item.title} - {item.count}  - ${item.price}  </h4>
+                <button className="btn btn-danger" onClick={ (e) => props.remove(e, item) } > Remove </button>
+            </li>
+        )
+    })
+    return (
+        cartitems
+    )
 }
 
 export default Cart;
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -6,46 +6,36 @@ class ProductList extends Component {
         super(props);
         this.state = {
             size: '',
-            count: ''
         }
     }
-
-    SelectSize = (e) =>{
-       // console.log(e.target.value);
+    SelectSize = (e) => {
+        // console.log(e.target.value);
         let selected = e.target.value;
-      //  console.log(selected);
+        //  console.log(selected);
 
         this.setState({
             size: selected,
         })
-        
-        
     }
 
-
-
     render() {
-     //   let style = { color: "#ffffff", h: 15 + "%"}
         const items = this.props.products.map(product => {
             let img = "img/" + product.sku + ".jpg";
-            return (<li key={product.id} className="col-md-8">
-                <img src={img} height="100" width="100" alt={product.title} />
-                <h4 style={{height: 15 + "%"}} > {product.title}</h4> <h5><b> ${product.price}</b> </h5>
-                <div style={{margin: 5 + "%", height: 10 + "%"}}>
-                    <SizeList availSizes={product.sizes} clicked={this.SelectSize} />
-                </div>
-                <button className="btn btn-primary" title="Add item to cart" onClick={ (e) => this.props.handleAddToCart(e, product, this.state.size) }>
-                    ADD TO CART
-                </button>
-                <h6 style = {{ display: "none" }}> Added to cart</h6>
-            </li>)
-
-        }
-
-        )
-        return (
-            items
-        )
+            return (
+                <li key={product.id} className="col-md-8">
+                    <img src={img} height="100" width="100" alt={product.title} />
+                    <h4 style={{ height: 15 + "%" }} > {product.title}</h4> <h5><b> ${product.price}</b> </h5>
+                    <div style={{ margin: 5 + "%", height: 10 + "%" }}>
+                        <SizeList availSizes={product.sizes} clicked={this.SelectSize} />
+                    </div>
+                    <button className="btn btn-primary" title="Add item to cart" onClick={ (e) => this.props.handleAddToCart(e, product, this.state.size) }>
+                        ADD TO CART
+                    </button>
+                    <h6 style = {{ display: "none" }}> Added to cart</h6>
+                </li>
+            )
+        })
+        return (items)
     }
 }
 
@@ -53,11 +43,7 @@ function SizeList(props) {
     let choose = props.availSizes.map((size, i) => {
         return <button className="btn btn-light" key={i} value={size} onClick={props.clicked}> {size} </button>
     });
-
-
-    return (
-        choose
-    )
+    return (choose)
 }
 
 //<select className="form-control" value={this.props.size} onChange={this.props.SelectSize} >
